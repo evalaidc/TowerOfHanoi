52228ca0cfa1abfaeec7c4dde267d2d2df217159
Eva Lai <evalai.dc@gmail.com>
2017-01-31 17:11:23 -0500

    tower of hanoi functional

--- project1/script.js
+++ project1/script.js
@@ -1,193 +1,44 @@
 //Pseudocode
-// start game
-// select the disc to be moved
 var selection = false;
+// select the cookie to be moved
 $('.post').on('click', function() {
-  // something is selected
+  // a cookie is selected
   if (selection == true) {
     // run the conditional
+      // value of selected box
     var value1 = $('.active').text();
+      // # in selected Post's top cookie
     var value2 = $(this).children(':first-child').text();
+    // checks to see if there is a cookie at the place already
+    // or if the cookie is small enough to go on top.
     if ($(this).children().length === 0 || +value1 < +value2) {
       $('.active').prependTo($(this))
       $('.active').removeClass('active');
       selection = false;
     }
+    // alerts you if a cookie is too large to go on the top cookie of the seleted place
     else  {
-      // console.log('This is not a valid move.')
       $('.active').removeClass('active');
       selection = false;
     }
   }
-  // if something was not selected
+  // if a cookie was not selected
   else {
     $(this).children(':first').addClass('active');
     selection = true;
   }
+  // if player gets all cookies in order to cookie monster.
+  // if ($('#post3').children().length === 4) {
+  //   setInterval(wonAlert(), 2000);
+  //   clearInterval()
+
+  // var won = setInterval(wonAlert(),5000);
+  setTimeout(function(){
+    if ($('#post3').children().length === 4) {
+    alert('You Won! The cookie monster thanks you for your help!')
+    }
+},3000)
+    // clearInterval();
+  // }
+
 });
-
-//      var width1 = $('.active').css('width');
-//      var width2 = this.children(':first-child').css('width');
-//      if (+width > +width2) {
-//          console.log('This is not a valid move.');
-//      }
-//      else {
-//        $('#post1').on('click', moveDisc)
-//        $('#post2').on('click', moveDisc)
-//        $('#post3').on('click', moveDisc)
-//      };
-//
-//
-//
-//
-//
-//
-//
-//
-//     if($(this).children(':first').hasClass('active')) {
-//       $(this).children(':first').removeClass('active')
-//     }
-//     else {
-//       // $('.disc').removeClass('active');
-//       $(this).children(':first').addClass('active');
-//       $('#post1').on('click', moveDisc)
-//       $('#post2').on('click', moveDisc)
-//       $('#post3').on('click', moveDisc)
-//   }
-// });
-//
-// // compare discs
-// if ($('.active').val() > $('#post2').children(':first').val()) {
-//   console.log('Not a Valid Move!');
-// }
-// else {
-//   $('.active').prependTo("#post2");
-// }
-//
-// if ($('.active').val() > $('#post3').children(':first').val()) {
-//   console.log('Not a Valid Move!');
-// }
-// else {
-//   $('.active').prependTo("#post3");
-// }
-//
-// if ($('.active').val() > $('#post1').children(':first').val()) {
-//   console.log('Not a Valid Move!');
-// }
-// else {
-//   $('.active').prependTo("#post1");
-// }
-
-
-// post1
-  //   if ($('#post1').hasClass('disc')) {
-  //     var value1 = $('.active').val();
-  //     var value2 = $('#post1').children(':first').val();
-  //     if (value1 > value2) {
-  //         console.log('This is not a valid move.');
-  //     }
-  //     else {
-  //       $('#post1').on('click', moveDisc)
-  //       $('#post2').on('click', moveDisc)
-  //       $('#post3').on('click', moveDisc)
-  //     };
-  //   }
-  //   else if ($('#post2').hasClass('disc')) {
-  //     var value1 = $('.active').val();
-  //     var value2 = $('#post2').children(':first').val();
-  //     if (value1 > value2) {
-  //         console.log('This is not a valid move.');
-  //     }
-  //     else {
-  //       $('#post1').on('click', moveDisc)
-  //       $('#post2').on('click', moveDisc)
-  //       $('#post3').on('click', moveDisc)
-  //     };
-  //   }
-  //   else if ($('#post3').hasClass('disc')) {
-  //     var value1 = $('.active').val();
-  //     var value2 = $('#post3').children(':first').val();
-  //     if (value1 > value2) {
-  //         console.log('This is not a valid move.');
-  //     }
-  //     else {
-  //       $('#post1').on('click', moveDisc)
-  //       $('#post2').on('click', moveDisc)
-  //       $('#post3').on('click', moveDisc)
-  //     };
-  //   }
-  // };
-// });
-
-//
-//
-// $('.post').on('click', gameInit)
-//
-// function selectDisc(){
-//   $(this).children(':first-child').addClass('active');
-// };
-// function deselectDisc(){
-//   $(this).children(':first-child').removeClass('active');
-// };
-// function anotherDisc() {
-//   if ($(this).hasClass('disc')) {
-//     var width1 = $('.active').css('width');
-//     var width2 = this.children(':first-child').css('width');
-//     if (+width > +width2) {
-//         console.log('This is not a valid move.');
-//     }
-//     else {
-//       $('#post1').on('click', moveDisc)
-//       $('#post2').on('click', moveDisc)
-//       $('#post3').on('click', moveDisc)
-//     };
-//   }
-//   else {
-//     $('#post1').on('click', moveDisc)
-//     $('#post2').on('click', moveDisc)
-//     $('#post3').on('click', moveDisc)
-//   }
-// };
-// function gameInit() {
-//   selectDisc();
-//   anotherDisc();
-//   deselectDisc();
-// }
-
-
-// how to connect post div to post div
-//   appendTo and prependTo to add discs to columns
-// $('#post1').on('click', moveDisc)
-// $('#post2').on('click', moveDisc)
-//
-// $('#post3').on('click', moveDisc)
-
-//set event listener to listen for a click on anywhere in the .post
-// function isThereContent() {
-//     if ($(this).hasClass('disc')) {
-//     $('.active').prependTo($(this));
-//     }
-//     else {
-//       moveDisc();
-//   };
-// };
-// function moveDisc() {
-//   $('.active').prependTo($(this));
-  // $('.active').removeClass('active');
-// }
-
-// function moveDisc(width1, width) {
-//   var width1 = $('.active');
-//   var width2 = $(this).children(':first-child');
-//   // to check if disc is larger, then it won't move.
-//   if ($(this).hasClass('disc')) {
-//     if (+width1.html() > +width2.html()) {
-//     console.log('This is not a valid move.');
-//     }
-//   else {
-//     $('.active').prependTo($(this));
-//     };
-//   };
-// };
-// if # value of disc > # of first-child disc, alert - cannot move item.
-  // else continue with append/prepend
